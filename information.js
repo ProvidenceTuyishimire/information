2bc01a473a75581c5013721bb0d32849f7173022
TuyishimireProvidence <Providencetuyishimire555@gmail.com>
2023-11-28 11:12:44 +0200

    update

---
+++ information.js
@@ -0,0 +1,39 @@
+const information1 = {
+    name: "Alice nziza",
+    phone: "078094567",
+    email: "alice.nziza@gmail.com"
+  };
+  
+  const information2 = {
+    name: "James Rwasa",
+    phone: "07956789",
+    email: "james.rwasa@gmail.com"
+  };
+  
+  const information3 = {
+    name: "Maria uwera",
+    phone: "078856780",
+    email: "maria.uwera@gmail.com"
+  };
+  
+  //information about the first contact
+  console.log("First information:");
+  console.log("Name: " + information1.name);
+  console.log("Phone: " + information1.phone);
+  console.log("Email: " + information1.email);
+  console.log("-----------");
+
+
+//information about the last contact
+console.log("second information:");
+console.log("Name: " + information2.name);
+console.log("Phone: " + information2.phone);
+console.log("Email: " + information2.email);
+console.log("-----------");
+  
+//information about the last contact
+console.log("Third information:");
+console.log("Name: " + information3.name);
+console.log("Phone: " + information3.phone);
+console.log("Email: " + information3.email);
+console.log("-----------");
